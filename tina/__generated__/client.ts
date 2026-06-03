054ccca77945385b4c36ea44a56216f09d0fb773
jnutterdev <hello@jnutterdev.com>
2026-06-03 02:06:17 -0400

    adding 404 page

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ cacheDir: '/Users/chromia/github.com/longlost-v3/tina/__generated__/.cache/1780466490363', url: 'https://content.tinajs.io/2.4/content/7e9a1db9-3ca9-439e-986d-328d5af5aae9/github/main', token: '4d1526e6b0114941b5b759cb667a82bfbefaafd2', queries,  });
+export const client = createClient({ cacheDir: '/Users/chromia/github.com/longlost-v3/tina/__generated__/.cache/1780466747624', url: 'https://content.tinajs.io/2.4/content/7e9a1db9-3ca9-439e-986d-328d5af5aae9/github/main', token: '4d1526e6b0114941b5b759cb667a82bfbefaafd2', queries,  });
 export default client;
   
