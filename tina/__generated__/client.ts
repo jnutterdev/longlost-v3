e9da1bcd49cd4e6e872843e3c43cdce082aa1e37
jnutterdev <hello@jnutterdev.com>
2026-06-03 00:09:32 -0400

    Adding favicon

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '4d1526e6b0114941b5b759cb667a82bfbefaafd2', queries,  });
 export default client;
   
--- tina/__generated__/config.prebuild.jsx
+++ tina/__generated__/config.prebuild.jsx
@@ -2,7 +2,7 @@
 import { defineConfig } from "tinacms";
 var config_default = defineConfig({
   branch: process.env.GITHUB_BRANCH ?? "main",
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID ?? "",
+  clientId: process.env.TINA_CLIENT_ID ?? "",
   token: process.env.TINA_TOKEN ?? "",
   build: {
     outputFolder: "admin",
